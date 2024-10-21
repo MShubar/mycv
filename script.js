26de0ef048ca734054e2e74cc44faa8bec423b66
MShubar <pokeeeeepow@gmail.com>
2024-10-21 15:58:44 +0300

    still

---
+++ script.js
@@ -0,0 +1,73 @@
+let phone = "39970100"
+let email = "mohsen.salman099@gmail.com"
+const address = { country: "Bahrain", area: "Tubli", block: "711" }
+const education = {
+  school: "Shaikh Abdul Aziz Secondary School",
+  university: "Arab Open University",
+}
+const experience = [
+  {
+    workplace: "Alsalahif Cold Store",
+    role: "Sales Officer",
+    startDate: { month: "12", year: "2018" },
+    endDate: { month: "2", year: "2022" },
+    area: "Hoora",
+    description: "",
+  },
+  {
+    workplace: "Al Kindi Hospital",
+    role: "",
+    startDate: { month: "2", year: "2022" },
+    endDate: { month: "3", year: "2022" },
+    area: "Zinj",
+    description: "",
+  },
+  {
+    workplace: "Silah stc",
+    role: "",
+    startDate: { month: "5", year: "2022" },
+    endDate: { month: "9", year: "2022" },
+    area: "Seef Area",
+    description: "",
+  },
+  {
+    workplace: "Silah stc",
+    role: "",
+    startDate: { month: "9", year: "2022" },
+    endDate: { month: "11", year: "2022" },
+    area: "Seef Area",
+    description: "",
+  },
+  {
+    workplace: "Silah stc",
+    role: "",
+    startDate: { month: "11", year: "2022" },
+    endDate: { month: "7", year: "2023" },
+    area: "Hoora",
+    description: "",
+  },
+  {
+    workplace: "Huawei stc",
+    role: "",
+    startDate: { month: "7", year: "2023" },
+    endDate: { month: "10", year: "2023" },
+    area: "Seef Area",
+    description: "",
+  },
+  {
+    workplace: "HMY Solutions",
+    role: "",
+    startDate: { month: "10", year: "2023" },
+    endDate: { month: "", year: "" },
+    area: "Keynsham, UK",
+    description: "",
+  },
+  {
+    workplace: "Bahrain Institute of Technology",
+    role: "",
+    startDate: { month: "12", year: "2023" },
+    endDate: { month: "", year: "" },
+    area: "Hoora",
+    description: "",
+  },
+]
